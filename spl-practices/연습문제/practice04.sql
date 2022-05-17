fc6906349089a1e04301709ecb6681087598057d
김현석 <gustjr324@gmail.com>
2022-05-17 18:22:42 +0900

    practice04 #2

--- spl-practices/연습문제/practice04.sql
+++ spl-practices/연습문제/practice04.sql
@@ -46,19 +46,26 @@ order by b.salary desc;
 -- 문제3.
 -- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
 -- 부서 평균
-select avg(salary)
+select * ,avg(salary)
 from departments a, dept_emp b, salaries c
 where a.dept_no = b.dept_no
 	and b.emp_no = c.emp_no
     and b.to_date = '9999-01-01'
     and c.to_date = '9999-01-01'
-group by b.dept_no;
+group by b.dept_no
+having c.salary > avg(salary);
 
-select a.emp_no, a.first_name, b.salary
-from employees a, salaries b
+select *
+from employees a, salaries b,  (select avg(salary) as sal
+from departments a, dept_emp b, salaries c
+where a.dept_no = b.dept_no
+	and b.emp_no = c.emp_no
+    and b.to_date = '9999-01-01'
+    and c.to_date = '9999-01-01'
+group by b.dept_no)
 where a.emp_no = b.emp_no
 	and b.to_date = '9999-01-01'
-    and b.salary > (select avg(salary)
+    and b.salary > (select avg(salary) as sal
 from departments a, dept_emp b, salaries c
 where a.dept_no = b.dept_no
 	and b.emp_no = c.emp_no
@@ -68,16 +75,83 @@ group by b.dept_no);
 
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
-
+select a.emp_no, a.first_name, c.dept_name
+from employees a,dept_manager b, departments c
+where a.emp_no = b.emp_no
+	and b.dept_no = c.dept_no
+	and b.to_date = '9999-01-01';
+    
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+select b.dept_no,avg(salary)
+from departments a, dept_emp b, salaries c
+where a.dept_no = b.dept_no
+	and b.emp_no = c.emp_no
+    and b.to_date = '9999-01-01'
+    and c.to_date = '9999-01-01'
+group by b.dept_no
+order by avg(salary) desc
+limit 0,1;
+
+select a.emp_no, a.first_name, b.title, c.salary, d.dept_no
+from employees a, titles b, salaries c, dept_emp d
+where a.emp_no = b.emp_no
+	and a.emp_no = c.emp_no
+    and a.emp_no = d.emp_no
+	and b.to_date = '9999-01-01'
+	and c.to_date = '9999-01-01'
+    and dept_no = (
+    select b.dept_no
+from departments a, dept_emp b, salaries c
+where a.dept_no = b.dept_no
+	and b.emp_no = c.emp_no
+    and b.to_date = '9999-01-01'
+    and c.to_date = '9999-01-01'
+group by b.dept_no
+order by avg(salary) desc
+limit 0,1)
+order by c.salary desc;
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
+select a.dept_name,avg(salary)
+from departments a, dept_emp b, salaries c
+where a.dept_no = b.dept_no
+	and b.emp_no = c.emp_no
+    -- and b.to_date = '9999-01-01'
+--     and c.to_date = '9999-01-01'
+group by b.dept_no
+order by avg(salary) desc
+limit 0,1;
 
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
-
+select a.title,avg(b.salary)
+from titles a, salaries b
+where a.emp_no = b.emp_no
+	-- and a.to_date = '9999-01-01'
+--     and b.to_date = '9999-01-01'
+group by title
+order by avg(b.salary) desc
+limit 0,1;
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다. 
+-- 현재 매니저들의 연봉
+select *
+from dept_manager a, salaries b
+where a.to_date = '9999-01-01'
+	and b.to_date = '9999-01-01'
+    and a.emp_no = b.emp_no;
+
+select a.dept_name, c.first_name, d.salary
+from departments a, dept_manager b, employees c, salaries d
+where a.dept_no = b.dept_no
+	and b.emp_no = c.emp_no
+	and c.emp_no = d.emp_no
+    and d.to_date = '9999-01-01'
+    and a.dept_no = 'd001'
+    and d.salary > '56654';
+
+
+
