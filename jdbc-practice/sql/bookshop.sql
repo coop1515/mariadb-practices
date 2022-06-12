b82bf9e364ed76155079b946d3730e522b501354
김현석 <gustjr324@gmail.com>
2022-06-12 14:10:24 +0900

    ..

--- jdbc-practice/sql/bookshop.sql
+++ jdbc-practice/sql/bookshop.sql
@@ -1,19 +1,24 @@
 -- bookshop
 
--- schema 확인
+-- schema 확인.
 desc book;
 desc author;
 
+-- AuthorDao:insert
+insert into author values (null, '원수연');
+
+-- select author
 select * from author;
 select * from book;
--- AuthorDao:insert
-insert into author values(null, '원수연');
 
--- BookDao: insert
--- insert into book values(null, '...','재고있음',1);
+delete from book;
+-- BookDao:insert
+-- insert into book values(null, 'title','재고있음',1);
 
--- BookDao: findAll()
+-- BookDao:findAll
 select a.no, a.title, b.name, a.state_code
 from book a, author b
 where a.author_no = b.no
-order by no asc;
+order by no;
+
+delete from author where no =2;
